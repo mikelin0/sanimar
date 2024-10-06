4c089a98f1fe1424da3b1cd5b051e9e3ba456140
mikelin0 <166568852+mikelin0@users.noreply.github.com>
2024-10-05 21:30:56 -0600

    Update lineas.js

--- lineas.js
+++ lineas.js
@@ -2,6 +2,5 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
 hamburger.addEventListener('click', () => {
-    console.log('Hamburger clicked'); // Esto te ayudará a verificar que el evento se activa
     navLinks.classList.toggle('open');
 });
